3431314ffd8bf9e1471479be6cb93e467b134d7e
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-10-26 14:59:06 +0200

    Configuring tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import './index.css';
 
 function App() {
   return (
-    <p className='text-white'>test</p>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
